b74dc0300f980879fd005351b8ec72f7595496e9
GGMaster <58326330+JyLu-GG@users.noreply.github.com>
2022-07-13 22:15:48 +0800

    upload file from web
    update main file, add doing Container with most water

--- src/leetcode/leetcodemain.go
+++ src/leetcode/leetcodemain.go
@@ -10,11 +10,21 @@ func main() {
 	fmt.Println("Leet Code Practice")
 	fmt.Println("")
 	fmt.Println("LeetCode Practice TwoSum")
-	input := []int{3,2,4}
+	input := []int{3, 2, 4}
 	target := 6
 	ans1 := leetcodepractice.TwoSum1(input, target)
 	ans2 := leetcodepractice.TwoSum2(input, target)
 	fmt.Println(ans1)
 	fmt.Println(ans2)
+	fmt.Println("")
+
+	fmt.Println("LeetCode Practice ContainerWithMostWater")
+	height := []int{2, 3, 4, 5, 18, 17, 6}
+	ans3 := leetcodepractice.MaxArea1(height)
+	ans4 := leetcodepractice.MaxArea2(height)
+	ans5 := leetcodepractice.MaxAreaFail(height)
+	fmt.Println(ans3)
+	fmt.Println(ans4)
+	fmt.Println(ans5)
 	fmt.Println("")
 }
